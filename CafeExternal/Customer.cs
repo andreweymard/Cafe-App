389392cb956b5dda4634e1624de0018fe9bf8189
Andrew Kevin <49222952+andreweymard@users.noreply.github.com>
2019-07-09 00:08:04 +0530

    Add files via upload

---
+++ CafeExternal/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment.CafeExternal
+{
+    class Customer
+    {
+        private string _Name;
+        public string Name { get { return _Name; } set { _Name = value; } }
+        private string _EmailAddress;
+        public string EmailAddress { get { return _EmailAddress; } set { _EmailAddress = value; } }
+        private UInt32 _TeleNumber;
+        public UInt32 TeleNumber { get { return _TeleNumber; } set { _TeleNumber = value; } }
+
+        public Customer(string name, string emailAddress, string teleNumber)
+        {
+            Name = name;
+            EmailAddress = emailAddress;
+            TeleNumber = Convert.ToUInt32(teleNumber);
+        }
+    }
+}
---
+++ CafeInternal/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment.CafeInternal
+{
+    public class Menu
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0,10}   |{1,20}   |{2,30}    |{3,10}", Code, Name, Description, Price);
+        }
+
+        public List<Menu> AddMenu()
+        {
+            var mi = new List<Menu>();
+
+            mi.Add(new Menu { Code = "D01", Name = "Cheesecake", Description = "A multi layered dessert.", Price = 300.00 });
+            mi.Add(new Menu { Code = "B01", Name = "Tea", Description = "A drink made from leaves.", Price = 100.00 });
+            mi.Add(new Menu { Code = "B02", Name = "Coffee", Description = "A drink made from beans.", Price = 600.00 });
+            mi.Add(new Menu { Code = "M01", Name = "Chicken Sandwich", Description = "A snack with chicken.", Price = 250.00 });
+
+            return mi;
+        }
+
+        public void Items()
+        {
+            Console.WriteLine("This is our menu:\n\n{0,10}   |{1,20}   |{2,30}    |{3,10}", "Code", "Name", "Description", "Price");
+            Console.WriteLine("     -------------------------------------------------------------------------------");
+
+            Menu menus = new Menu();
+            foreach (Menu amenu in menus.AddMenu())
+            {
+                Console.WriteLine(amenu);
+            }
+        }
+    }
+}
